e5daf019ad7bd5b6aa8494bf936a913bbfa38f51
Ugho STEPHAN <ugho.stephan@payfit.com>
2022-12-10 16:32:05 +0100

    feat: implement admin page

--- apps/front/src/components/admin/AdminPage.tsx
+++ apps/front/src/components/admin/AdminPage.tsx
@@ -1,11 +1,49 @@
-import { Box } from '@mui/material';
+import { Box, Tab, Tabs } from '@mui/material';
 import React from 'react';
 import { Title } from '../common/Title';
+import { useCustomSearchParams } from '@wishlist/common-front';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import GroupsIcon from '@mui/icons-material/Groups';
+
+enum TabValues {
+  users = 'users',
+  events = 'events',
+}
+
+const tabs = [
+  {
+    value: TabValues.users,
+    label: 'Utilisateurs',
+    icon: <GroupsIcon />,
+  },
+  {
+    value: TabValues.events,
+    label: 'Évènements',
+    icon: <CalendarMonthIcon />,
+  },
+];
+
+type SearchParamType = { tab: TabValues };
 
 export const AdminPage = () => {
+  const [queryParams, setQueryParams] = useCustomSearchParams<SearchParamType>({ tab: tabs[0].value });
+
   return (
     <Box>
-      <Title>Administration</Title>
+      <Title smallMarginBottom>Administration</Title>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', marginBottom: '20px' }}>
+        <Tabs
+          value={queryParams.tab}
+          onChange={(_, newValue) => setQueryParams({ tab: newValue })}
+          variant="fullWidth"
+          scrollButtons="auto"
+          allowScrollButtonsMobile
+        >
+          {tabs.map((tab) => (
+            <Tab key={tab.value} value={tab.value} iconPosition="start" icon={tab.icon} label={tab.label} />
+          ))}
+        </Tabs>
+      </Box>
     </Box>
   );
 };
