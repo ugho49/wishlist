b6814d54499e3726c503d6385dc7e2b7a8364403
Ugho STEPHAN <ugho.stephan@payfit.com>
2022-12-27 10:51:12 +0100

    fix: prod front

--- apps/front/src/environments/environment.prod.ts
+++ apps/front/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Environment } from '../@types/environment.type';
 
 export const environment: Environment = {
   production: true,
-  baseUrl: 'http://localhost:8080',
+  baseUrl: 'https://api.wishlistapp.fr',
 };
