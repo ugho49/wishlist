78d92ac3dc519b0851b6f124edaad178a9ab5cb3
Ugho STEPHAN <ugho.stephan@payfit.com>
2023-01-05 20:37:16 +0100

    feat: force redirect front if wrong hostname

--- apps/front/src/main.tsx
+++ apps/front/src/main.tsx
@@ -15,27 +15,34 @@ import { environment } from './environments/environment';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-root.render(
-  <ApiProvider apis={apis}>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <LocalizationProvider dateAdapter={AdapterLuxon} adapterLocale="fr">
-          <BrowserRouter>
-            <SnackbarProvider
-              maxSnack={3}
-              autoHideDuration={3_000}
-              anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-              preventDuplicate
-            >
-              <AxiosInterceptor />
-              <GoogleOAuthProvider clientId={environment.googleClientId}>
-                <App />
-              </GoogleOAuthProvider>
-            </SnackbarProvider>
-          </BrowserRouter>
-        </LocalizationProvider>
-      </ThemeProvider>
-    </Provider>
-  </ApiProvider>
-);
+if (
+  window.location.hostname === 'wishlist-stephan.web.app' ||
+  window.location.hostname === 'wishlist-stephan.firebaseapp.com'
+) {
+  window.location.href = 'https://wishlistapp.fr';
+} else {
+  root.render(
+    <ApiProvider apis={apis}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <LocalizationProvider dateAdapter={AdapterLuxon} adapterLocale="fr">
+            <BrowserRouter>
+              <SnackbarProvider
+                maxSnack={3}
+                autoHideDuration={3_000}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                preventDuplicate
+              >
+                <AxiosInterceptor />
+                <GoogleOAuthProvider clientId={environment.googleClientId}>
+                  <App />
+                </GoogleOAuthProvider>
+              </SnackbarProvider>
+            </BrowserRouter>
+          </LocalizationProvider>
+        </ThemeProvider>
+      </Provider>
+    </ApiProvider>
+  );
+}
