a460555f930b7e39401a9b8dd34ac6f2f2fb8133
Ugho STEPHAN <ugho.stephan@payfit.com>
2023-02-11 14:24:41 +0100

    feat: use Jimp instead of Sharp to resize images

--- apps/api/src/core/bucket/resize-image.pipe.ts
+++ apps/api/src/core/bucket/resize-image.pipe.ts
@@ -1,21 +1,20 @@
 import { PipeTransform } from '@nestjs/common';
-import * as sharp from 'sharp';
+import * as Jimp from 'jimp';
 
 type OptionsResizeImagePipe = { width: number; height: number };
 
-export class ResizeImagePipe implements PipeTransform<Express.Multer.File, Promise<string>> {
+export class ResizeImagePipe implements PipeTransform<Express.Multer.File> {
   constructor(private readonly options: OptionsResizeImagePipe) {}
 
-  async transform(image: Express.Multer.File): Promise<any> {
-    const buffer = await sharp(image.buffer, { failOn: 'none' })
-      .resize({
-        width: this.options.width,
-        height: this.options.height,
-        fit: 'cover',
-      })
-      .webp({ effort: 3 })
-      .toBuffer();
-
-    return { ...image, buffer, mimetype: 'image/webp' };
+  async transform(image: Express.Multer.File): Promise<Express.Multer.File> {
+    try {
+      const buffer = await Jimp.read(image.buffer).then((img) =>
+        img.cover(this.options.width, this.options.height).quality(60).getBufferAsync(Jimp.MIME_JPEG)
+      );
+      return { ...image, buffer, mimetype: Jimp.MIME_JPEG };
+    } catch (e) {
+      console.error('Fail to resize image', e);
+      return image;
+    }
   }
 }
