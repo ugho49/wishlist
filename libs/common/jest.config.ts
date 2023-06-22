9e24109d3199517bd304774467719a97cec62b39
Ugho STEPHAN <ugho.stephan@payfit.com>
2023-06-22 14:50:28 +0200

    fix: tests

--- libs/common/jest.config.ts
+++ libs/common/jest.config.ts
@@ -2,13 +2,9 @@
 export default {
   displayName: 'common',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/common',
