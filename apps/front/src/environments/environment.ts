da496f9121f14c0b209a4a3d719afc76b0846efe
Ugho STEPHAN <ugho.stephan@payfit.com>
2023-11-11 19:14:42 +0100

    fix: dev env

--- apps/front/src/environments/environment.ts
+++ apps/front/src/environments/environment.ts
@@ -5,6 +5,6 @@ import { Environment } from '../@types/environment.type';
 
 export const environment: Environment = {
   production: false,
-  baseUrl: 'https://api.wishlistapp.fr',
+  baseUrl: 'http://localhost:8080',
   googleClientId: '746810988919-huhupt1qtdliht08e5s5m4f5ruqueemb.apps.googleusercontent.com',
 };
