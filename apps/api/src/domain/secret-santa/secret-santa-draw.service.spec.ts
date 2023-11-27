76cb3cecad0c76aa8281e4d93e649b3a71f1dfaa
Ugho STEPHAN <ugho.stephan@payfit.com>
2023-11-27 11:29:30 +0100

    chore: add tests

--- apps/api/src/domain/secret-santa/secret-santa-draw.service.spec.ts
+++ apps/api/src/domain/secret-santa/secret-santa-draw.service.spec.ts
@@ -1,5 +1,7 @@
 import { SecretSantaDrawService } from './secret-santa-draw.service';
 import { SecretSantaUserEntity } from './secret-santa.entity';
+import { toIncludeAllMembers, toBeArrayOfSize } from 'jest-extended';
+expect.extend({ toIncludeAllMembers, toBeArrayOfSize });
 
 describe('SecretSantaDrawService', () => {
   const drawService = new SecretSantaDrawService();
@@ -22,8 +24,10 @@ describe('SecretSantaDrawService', () => {
     const assignedUsers = drawService.assignSecretSantas(secretSantaUsers);
 
     // Then
-    expect(assignedUsers[0]).toEqual({ userId: '1', drawUserId: '2' });
-    expect(assignedUsers[1]).toEqual({ userId: '2', drawUserId: '1' });
+    expect(assignedUsers).toIncludeAllMembers([
+      { userId: '1', drawUserId: '2' },
+      { userId: '2', drawUserId: '1' },
+    ]);
   });
 
   it('should throw error when secret Santas for 2 attendees with exceptions', () => {
@@ -48,9 +52,55 @@ describe('SecretSantaDrawService', () => {
     const assignedUsers = drawService.assignSecretSantas(secretSantaUsers);
 
     // Then
-    expect(assignedUsers[0].drawUserId).toEqual('3');
-    expect(assignedUsers[1].drawUserId).toEqual('1');
-    expect(assignedUsers[2].drawUserId).toEqual('2');
+    expect(assignedUsers).toIncludeAllMembers([
+      { userId: '1', drawUserId: '3' },
+      { userId: '2', drawUserId: '1' },
+      { userId: '3', drawUserId: '2' },
+    ]);
+  });
+
+  it('should throw error if 2 attendee have the same uniq draw', () => {
+    // Given
+    const secretSantaUsers: SecretSantaUserEntity[] = [
+      getUserEntity('1', []),
+      getUserEntity('2', ['1']),
+      getUserEntity('3', ['1']),
+    ];
+
+    // When Then
+    expect(() => drawService.assignSecretSantas(secretSantaUsers)).toThrowError(
+      'Impossible de tirer au sort un utilisateur en raison des exclusions.',
+    );
+  });
+
+  it('should throw error if 3 attendee have the same uniq draw', () => {
+    // Given
+    const secretSantaUsers: SecretSantaUserEntity[] = [
+      getUserEntity('1', []),
+      getUserEntity('2', ['1']),
+      getUserEntity('3', ['1']),
+      getUserEntity('4', ['1']),
+    ];
+
+    // When Then
+    expect(() => drawService.assignSecretSantas(secretSantaUsers)).toThrowError(
+      'Impossible de tirer au sort un utilisateur en raison des exclusions.',
+    );
+  });
+
+  it('should assign secret Santas for 4 attendees', () => {
+    // Given
+    const secretSantaUsers: SecretSantaUserEntity[] = [
+      getUserEntity('1', []),
+      getUserEntity('2', []),
+      getUserEntity('3', []),
+      getUserEntity('4', []),
+    ];
+
+    // When
+    const assignedUsers = drawService.assignSecretSantas(secretSantaUsers);
+
+    expect(assignedUsers).toBeArrayOfSize(4);
   });
 });
 
--- apps/api/src/domain/secret-santa/secret-santa-draw.service.ts
+++ apps/api/src/domain/secret-santa/secret-santa-draw.service.ts
@@ -3,69 +3,63 @@ import { SecretSantaUserEntity } from './secret-santa.entity';
 type SecretSantaAssign = { userId: string; drawUserId: string };
 
 export class SecretSantaDrawService {
-  private isValidDraw(user: SecretSantaUserEntity, drawUser: SecretSantaUserEntity): boolean {
-    return !user.exclusions.includes(drawUser.id);
-  }
-
-  private generateAllPossibleDraws(users: SecretSantaUserEntity[]): SecretSantaUserEntity[][] {
-    if (users.length <= 1) {
-      return [users];
-    }
-
-    const [first, ...rest] = users;
-    const restDraws = this.generateAllPossibleDraws(rest);
-
-    return restDraws.flatMap((draw) => {
-      return Array.from({ length: draw.length + 1 }, (_, index) => {
-        const drawCopy = draw.slice();
-        drawCopy.splice(index, 0, first);
-        return drawCopy;
-      });
-    });
-  }
-
-  private findValidDraw(users: SecretSantaUserEntity[]): SecretSantaUserEntity[] | undefined {
-    const possibleDraws = this.generateAllPossibleDraws(users);
-
-    for (const draw of possibleDraws) {
-      let valid = true;
-
-      for (let i = 0; i < users.length; i++) {
-        if (!this.isValidDraw(users[i], draw[i])) {
-          valid = false;
-          break;
-        }
-      }
-
-      if (valid) {
-        return draw;
-      }
-    }
-
-    return undefined;
-  }
-
-  public assignSecretSantas(secretSantaUsers: SecretSantaUserEntity[]): SecretSantaAssign[] {
-    if (secretSantaUsers.length < 2) {
+  public assignSecretSantas(users: SecretSantaUserEntity[]): SecretSantaAssign[] {
+    if (users.length < 2) {
       throw new Error("Pas assez d'utilisateurs pour tirer au sort.");
     }
 
-    const sortedUsers = [...secretSantaUsers].sort((a, b) => a.exclusions.length - b.exclusions.length);
+    const possibleDraws = this.getPossibleDraws(users);
 
+    return this.draw(possibleDraws);
+  }
+
+  private getPossibleDraws(users: SecretSantaUserEntity[]) {
+    const sortedUsers = [...users].sort((a, b) => a.exclusions.length - b.exclusions.length);
     sortedUsers.forEach((user) => (user.drawUserId = undefined));
 
-    const validDraw = this.findValidDraw(sortedUsers);
+    const possibleDraws: { userId: string; possibleDrawIds: string[] }[] = [];
+    const drawUserIds = new Set<string>();
 
-    if (!validDraw) {
+    for (const user of sortedUsers) {
+      const exclusions = [user.id, ...user.exclusions];
+      const possibleDrawIds = sortedUsers.filter((u) => !exclusions.includes(u.id)).map((u) => u.id);
+
+      if (possibleDrawIds.length === 0) {
+        throw new Error('Impossible de tirer au sort un utilisateur en raison des exclusions.');
+      }
+
+      possibleDrawIds.forEach((id) => drawUserIds.add(id));
+      possibleDraws.push({ userId: user.id, possibleDrawIds });
+    }
+
+    if (drawUserIds.size !== users.length) {
       throw new Error('Impossible de tirer au sort un utilisateur en raison des exclusions.');
     }
 
-    const result: SecretSantaAssign[] = [];
+    return possibleDraws;
+  }
 
-    for (let i = 0; i < sortedUsers.length; i++) {
-      result.push({ userId: sortedUsers[i].id, drawUserId: validDraw[i].id });
+  private draw(
+    possibleDraws: { userId: string; possibleDrawIds: string[] }[],
+    retryCount: number = 0,
+  ): SecretSantaAssign[] {
+    const finalDraws: SecretSantaAssign[] = [];
+    const drawnIds: string[] = [];
+
+    if (retryCount > 10) {
+      throw new Error('Trop de tentatives de tirage au sort, le tirage ne semble pas correcte.');
     }
 
-    return result;
+    for (const possibleDraw of possibleDraws) {
+      const possibleDrawIds = possibleDraw.possibleDrawIds.filter((id) => !drawnIds.includes(id));
+      if (possibleDrawIds.length === 0) {
+        return this.draw(possibleDraws, retryCount + 1);
+      }
+      const drawUserId = possibleDrawIds[Math.floor(Math.random() * possibleDrawIds.length)];
+      drawnIds.push(drawUserId);
+      finalDraws.push({ userId: possibleDraw.userId, drawUserId });
+    }
+
+    return finalDraws;
   }
 }
