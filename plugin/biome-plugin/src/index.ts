7421b0de5b10e3e00f1f395bc17ddb9daee71db6
Ugho STEPHAN <stephan.ugho@gmail.com>
2025-10-15 21:04:39 +0200

    chore: update biome conf

--- plugin/biome-plugin/src/index.ts
+++ plugin/biome-plugin/src/index.ts
@@ -1,4 +1,5 @@
-import { dirname } from 'path'
+import { existsSync } from 'fs'
+import { basename, dirname, join } from 'path'
 import { type CreateNodesV2, createNodesFromFiles } from '@nx/devkit'
 
 export type BiomePluginOptions = {
@@ -8,10 +9,19 @@ export type BiomePluginOptions = {
 }
 
 export const createNodesV2: CreateNodesV2<BiomePluginOptions> = [
-  '**/biome.json',
+  '**/biome.{json,jsonc}',
   async (configFiles, options, context) => {
+    // Get root biome.json or biome.jsonc path
+    // const rootBiomeJsonAbsolutePath = context.workspaceRoot.find(
+    //   file => file.endsWith('biome.json') || file.endsWith('biome.jsonc'),
+    // )
+    const rootBiomeJsonAbsolutePath = [
+      join(context.workspaceRoot, 'biome.json'),
+      join(context.workspaceRoot, 'biome.jsonc'),
+    ].find(file => existsSync(file))
+
     return await createNodesFromFiles(
-      configFile => createNodesInternal(configFile, options),
+      configFile => createNodesInternal({ configFilePath: configFile, options, rootBiomeJsonAbsolutePath }),
       configFiles,
       options,
       context,
@@ -19,19 +29,31 @@ export const createNodesV2: CreateNodesV2<BiomePluginOptions> = [
   },
 ]
 
-function createNodesInternal(configFilePath: string, options?: BiomePluginOptions) {
+function createNodesInternal(params: {
+  configFilePath: string
+  options?: BiomePluginOptions
+  rootBiomeJsonAbsolutePath?: string
+}) {
+  const { configFilePath, options, rootBiomeJsonAbsolutePath } = params
   const root = dirname(configFilePath)
 
-  // because there is also a biome.json at the root of the workspace, we want to ignore that one
+  // because there is also a biome.json/biome.jsonc at the root of the workspace, we want to ignore that one
   // return an empty object if we're at the root so that we don't create a root project
 
   if (root === '.') {
     return {}
   }
 
+  const inputs = [
+    'default',
+    '^default',
+    ...(rootBiomeJsonAbsolutePath ? [`{workspaceRoot}/${basename(rootBiomeJsonAbsolutePath)}`] : []),
+    { externalDependencies: ['@biomejs/biome'] },
+  ]
+
   const base = {
     cache: true,
-    inputs: ['default', '^default', '{workspaceRoot}/biome.json', { externalDependencies: ['@biomejs/biome'] }],
+    inputs,
   }
 
   // Project configuration to be merged into the rest of the Nx configuration
