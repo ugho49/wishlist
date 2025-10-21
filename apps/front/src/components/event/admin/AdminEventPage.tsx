37717bfa1b833f5fdb624be92e216cc53efbe53d
Ugho STEPHAN <stephan.ugho@gmail.com>
2025-10-21 19:57:45 +0200

    feat(front): implement update event in admin

--- apps/front/src/components/event/admin/AdminEventPage.tsx
+++ apps/front/src/components/event/admin/AdminEventPage.tsx
@@ -1,22 +1,32 @@
-import type { AttendeeId, EventId, SecretSantaUserId, UpdateSecretSantaInputDto } from '@wishlist/common'
+import type {
+  AttendeeId,
+  EventId,
+  SecretSantaUserId,
+  UpdateEventInputDto,
+  UpdateSecretSantaInputDto,
+} from '@wishlist/common'
 import type { AxiosError } from 'axios'
 
+import { zodResolver } from '@hookform/resolvers/zod'
 import AccessTimeIcon from '@mui/icons-material/AccessTime'
+import DeleteIcon from '@mui/icons-material/Delete'
 import LocalPoliceOutlinedIcon from '@mui/icons-material/LocalPoliceOutlined'
-import NumbersIcon from '@mui/icons-material/Numbers'
-import ShortTextIcon from '@mui/icons-material/ShortText'
-import TitleIcon from '@mui/icons-material/Title'
-import { Box, List, ListItem, ListItemIcon, ListItemText, Stack } from '@mui/material'
+import SaveIcon from '@mui/icons-material/Save'
+import { Box, Button, List, ListItem, ListItemIcon, ListItemText, Stack, TextField } from '@mui/material'
 import { styled, useTheme } from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { AttendeeRole } from '@wishlist/common'
+import { ConfirmButton } from '@wishlist/front-components/common/ConfirmButton'
+import { WishlistDatePicker } from '@wishlist/front-components/common/DatePicker'
+import { EmojiSelectorWithBadge } from '@wishlist/front-components/common/EmojiSelectorWithBadge'
 import { DateTime } from 'luxon'
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
+import { Controller, useForm } from 'react-hook-form'
 import { useParams } from 'react-router-dom'
+import z from 'zod'
 
 import { useApi, useToast } from '../../../hooks'
-import { BreaklineText } from '../../common/BreaklineText'
 import { CardV2 } from '../../common/CardV2'
 import { Loader } from '../../common/Loader'
 import { Subtitle } from '../../common/Subtitle'
@@ -29,6 +39,18 @@ const CardStack = styled(Stack)(() => ({
   gap: 32,
 }))
 
+const schema = z.object({
+  icon: z.string().optional(),
+  title: z.string().min(1, 'Le titre est requis').max(100, '100 caractères maximum'),
+  description: z.string().max(2000, '2000 caractères maximum').optional(),
+  eventDate: z
+    .custom<DateTime>()
+    .nullable()
+    .refine(date => date !== null, "La date de l'événement est requise"),
+})
+
+type FormFields = z.infer<typeof schema>
+
 export const AdminEventPage = () => {
   const params = useParams<'eventId'>()
   const eventId = (params.eventId || '') as EventId
@@ -37,6 +59,16 @@ export const AdminEventPage = () => {
   const { addToast } = useToast()
   const theme = useTheme()
   const smallScreen = useMediaQuery(theme.breakpoints.down('md'))
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<FormFields>({
+    resolver: zodResolver(schema),
+  })
 
   const { data: event, isLoading: loadingEvent } = useQuery({
     queryKey: ['admin', 'event', { eventId }],
@@ -114,6 +146,32 @@ export const AdminEventPage = () => {
     onError: () => addToast({ message: 'Impossible de modifier le secret santa', variant: 'error' }),
   })
 
+  const { mutateAsync: updateEvent, isPending: loadingUpdateEvent } = useMutation({
+    mutationKey: ['admin', 'event.update', { eventId }],
+    mutationFn: (input: UpdateEventInputDto) => api.event.update(eventId, input),
+    onSuccess: () => {
+      addToast({ message: 'Évènement modifié avec succès', variant: 'success' })
+      queryClient.invalidateQueries({ queryKey: ['admin', 'event', { eventId }] })
+    },
+    onError: () => addToast({ message: "Impossible de modifier l'évènement", variant: 'error' }),
+  })
+
+  const { mutateAsync: deleteEvent, isPending: loadingDeleteEvent } = useMutation({
+    mutationKey: ['admin', 'event.delete', { eventId }],
+    mutationFn: () => api.event.delete(eventId),
+    onSuccess: () => {
+      addToast({ message: 'Évènement supprimé avec succès', variant: 'success' })
+      queryClient.invalidateQueries({ queryKey: ['admin', 'event', { eventId }] })
+    },
+    onError: error => {
+      const axiosError = error as AxiosError<{ message: string }>
+      addToast({
+        message: `Impossible de supprimer l'évènement: ${axiosError.response?.data.message}`,
+        variant: 'error',
+      })
+    },
+  })
+
   const maintainerName = useMemo(() => {
     if (!event) return ''
     const user = event.attendees.find(attendee => attendee.role === AttendeeRole.MAINTAINER)
@@ -121,13 +179,35 @@ export const AdminEventPage = () => {
     return `${user.user?.firstname} ${user.user?.lastname}`
   }, [event])
 
+  useEffect(() => {
+    if (event) {
+      setValue('title', event.title)
+      setValue('description', event.description)
+      setValue('eventDate', DateTime.fromISO(event.event_date))
+      setValue('icon', event.icon)
+    }
+  }, [event, setValue])
+
+  const onSubmit = async (data: FormFields) => {
+    const isoDate = data.eventDate!.toISODate()!
+    const body: UpdateEventInputDto = {
+      title: data.title,
+      description: data.description === '' ? undefined : data.description,
+      icon: data.icon,
+      event_date: new Date(isoDate),
+    }
+    await updateEvent(body)
+  }
+
   const loadingEdit =
     loadingDeleteSecretSanta ||
     loadingStartSecretSanta ||
     loadingCancelSecretSanta ||
     loadingRemoveSecretSantaUser ||
     loadingUpdateSecretSanta ||
-    loadingDeleteAttendee
+    loadingDeleteAttendee ||
+    loadingUpdateEvent ||
+    loadingDeleteEvent
 
   return (
     <Box>
@@ -136,21 +216,8 @@ export const AdminEventPage = () => {
       <CardStack>
         <CardV2>
           <Loader loading={loadingEvent}>
-            <Subtitle>Détails</Subtitle>
             <Stack direction="row" flexWrap="wrap" gap={smallScreen ? 0 : 3}>
               <List dense sx={{ flexGrow: 1 }}>
-                <ListItem>
-                  <ListItemIcon>
-                    <TitleIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Titre" secondary={event?.title} />
-                </ListItem>
-                <ListItem>
-                  <ListItemIcon>
-                    <NumbersIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Nombre de participants" secondary={event?.attendees.length} />
-                </ListItem>
                 <ListItem>
                   <ListItemIcon>
                     <LocalPoliceOutlinedIcon />
@@ -171,27 +238,102 @@ export const AdminEventPage = () => {
                     )}
                   />
                 </ListItem>
-                <ListItem>
-                  <ListItemIcon>
-                    <NumbersIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Nombre de listes" secondary={event?.wishlists.length} />
-                </ListItem>
               </List>
-            </Stack>
-            {event?.description && (
-              <Stack direction="row" flexWrap="wrap" gap={smallScreen ? 0 : 3}>
-                <List dense sx={{ flexGrow: 1 }}>
-                  <ListItem>
-                    <ListItemIcon>
-                      <ShortTextIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Description" secondary={<BreaklineText text={event?.description ?? ''} />} />
-                  </ListItem>
-                </List>
+
+              <Stack sx={{ flexGrow: 1 }} alignItems="center" justifyContent="center">
+                <ConfirmButton
+                  confirmTitle="Supprimer l'évènement"
+                  confirmText="Etes vous sûr de supprimer l'évènement ? Cela supprimera toutes les listes associés !"
+                  variant="outlined"
+                  color="error"
+                  size="small"
+                  startIcon={<DeleteIcon />}
+                  onClick={() => deleteEvent()}
+                >
+                  Supprimer l'évènement
+                </ConfirmButton>
               </Stack>
-            )}
+            </Stack>
           </Loader>
+        </CardV2>
+
+        <CardV2>
+          <Subtitle>Modifier les informations</Subtitle>
+
+          <Stack component="form" onSubmit={handleSubmit(onSubmit)} gap={3}>
+            <Stack direction="row" gap={3} alignItems="flex-start">
+              <Controller
+                control={control}
+                name="icon"
+                render={({ field }) => (
+                  <EmojiSelectorWithBadge value={field.value} onChange={value => field.onChange(value)} />
+                )}
+              />
+              <Box sx={{ flex: 1 }}>
+                <TextField
+                  {...register('title')}
+                  label="Titre"
+                  autoComplete="off"
+                  disabled={loadingEdit}
+                  fullWidth
+                  placeholder="Le titre de votre évènement"
+                  error={!!errors.title}
+                  helperText={errors.title?.message}
+                />
+              </Box>
+            </Stack>
+
+            <Box>
+              <TextField
+                {...register('description')}
+                label="Description"
+                autoComplete="off"
+                disabled={loadingEdit}
+                fullWidth
+                multiline
+                minRows={4}
+                placeholder="Une petite description ..."
+                error={!!errors.description}
+                helperText={errors.description?.message}
+              />
+            </Box>
+
+            <Stack>
+              <Controller
+                control={control}
+                name="eventDate"
+                render={({ field }) => (
+                  <WishlistDatePicker
+                    label="Date de l'évènement"
+                    format="DDDD"
+                    value={field.value}
+                    inputRef={field.ref}
+                    disabled={loadingEdit}
+                    onChange={date => field.onChange(date)}
+                    disablePast
+                    fullWidth
+                    error={!!errors.eventDate}
+                    helperText={errors.eventDate?.message}
+                  />
+                )}
+              />
+            </Stack>
+
+            <Stack direction="row" justifyContent="center">
+              <Button
+                sx={{ marginTop: '8px' }}
+                type="submit"
+                variant="contained"
+                size="small"
+                loading={loadingEdit}
+                loadingPosition="start"
+                disabled={loadingEdit}
+                startIcon={<SaveIcon />}
+              >
+                Mettre à jour
+              </Button>
+            </Stack>
+          </Stack>
         </CardV2>
 
         <CardV2>
