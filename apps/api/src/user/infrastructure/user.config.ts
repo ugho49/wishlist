d2a7dd24790975fcf530aa814c61726eb36e8763
Ugho STEPHAN <stephan.ugho@gmail.com>
2025-11-17 17:20:15 +0100

    chore(back): validate config with zod

--- apps/api/src/user/infrastructure/user.config.ts
+++ apps/api/src/user/infrastructure/user.config.ts
@@ -1,7 +1,22 @@
 import { registerAs } from '@nestjs/config'
+import { z } from 'zod'
+
+const schema = z.object({
+  RESET_PASSWORD_TOKEN_DURATION_IN_MIN: z.coerce.number(
+    'Missing RESET_PASSWORD_TOKEN_DURATION_IN_MIN environment variable',
+  ),
+})
 
 export default registerAs('user', () => {
+  const result = schema.safeParse(process.env)
+
+  if (!result.success) {
+    throw new Error(z.prettifyError(result.error))
+  }
+
+  const validatedConfig = result.data
+
   return {
-    resetPasswordTokenDurationInMinutes: parseInt(process.env.RESET_PASSWORD_TOKEN_DURATION_IN_MIN || '15', 10),
+    resetPasswordTokenDurationInMinutes: validatedConfig.RESET_PASSWORD_TOKEN_DURATION_IN_MIN,
   }
 })
